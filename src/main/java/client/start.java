92ab0a62593f4a9823931701d042be6af68763f3
Walter Benjamin <walterbenjamin.87@gmail.com>
2019-09-04 14:12:48 +0530

    added entry class

---
+++ src/main/java/client/start.java
@@ -0,0 +1,28 @@
+package hello;
+import java.util.*;
+import org.apache.pulsar.client.admin.PulsarAdmin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Greeter {
+  public String sayHello(String namespace) {
+          int i=0;
+      try{
+          PulsarAdmin admin = PulsarAdmin.builder().serviceHttpUrl("http://13.74.20.87:8080").build();
+          //namespace tenant01/namespace01
+          System.out.println(namespace);
+          List<String> topiclist = admin.topics().getList(namespace);
+          for(String s:topiclist){
+                  admin.topics().delete(s,true);
+                  System.out.println(s);
+                  i++;
+          }
+      }catch(Exception e){
+              System.err.println("Exce:::" + e);
+      }
+      //admin.close();
+      System.out.println(i);
+      return "Hello world!";
+  }
+}
